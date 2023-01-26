d5890f1a75121777eb156d2ef09c154b653cb84c
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-01-25 18:30:11 -0800

    still deleting

--- COMP003A.LectureActivity2/Program.cs
+++ COMP003A.LectureActivity2/Program.cs
@@ -26,41 +26,41 @@ Console.WriteLine("Hi " + username); // retrieving its current value (3 of 3)
 // another example of the three primary variable related activities (declaring, assigning, and retrieving)
 string favoriteColor;
 Console.WriteLine("What is your favorite color?");
-25   favoriteColor = Console.ReadLine();
-26   Console.WriteLine("Your favorite color is " + favoriteColor);
-27   
-28   
-29   /* working with variable integers */
-30   int score; // declare a new variable with a data type int
-31   // score = "Generic User"; // will fail to compile because you are assigning a string value to an integer variable
-32   // score = "0"; // will still fail to compile because the "0" is still assigned as a string - enclosed in the double quotes
-33   score = 0; // works
-34   score = 4; // you can also reassign values to the variable as long as they are within scope and not constant
-35   score = 11;
-36   score = - 1564;
-37   
-38   
-39   /* reading from a variable does not change it */
-40   int a;
-41   int b;
-42   
-43   a = 5;
-44   b = 2;
-45   Console.WriteLine("The current value of a: " + a);
-46   Console.WriteLine("The current value of b: " + b);
-47   
-48   b = a;
-49   a = - 3;
-50   Console.WriteLine("The new value of a: " + a);
-51   Console.WriteLine("The new value of b: " + b);
-52   
-53   /* variables are case-sensitive */
-54   // the variables below are different despite having similar names
-55   string exampleVariable;
-56   string exampleVAriable;
-57   string exampleVARiable;
-58   
-59   Console.WriteLine("**************************************************");
+favoriteColor = Console.ReadLine();
+Console.WriteLine("Your favorite color is " + favoriteColor);
+
+   
+/* working with variable integers */
+int score; // declare a new variable with a data type int
+// score = "Generic User"; // will fail to compile because you are assigning a string value to an integer variable
+// score = "0"; // will still fail to compile because the "0" is still assigned as a string - enclosed in the double quotes
+score = 0; // works
+score = 4; // you can also reassign values to the variable as long as they are within scope and not constant
+score = 11;
+score = - 1564;
+  
+   
+/* reading from a variable does not change it */
+int a;
+int b;
+   
+a = 5;
+b = 2;
+Console.WriteLine("The current value of a: " + a);
+Console.WriteLine("The current value of b: " + b);
+   
+b = a;
+a = - 3;
+Console.WriteLine("The new value of a: " + a);
+Console.WriteLine("The new value of b: " + b);
+
+/* variables are case-sensitive */
+// the variables below are different despite having similar names
+string exampleVariable;
+string exampleVAriable;
+string exampleVARiable;
+
+Console.WriteLine("**************************************************");
 60   /*
 61                * types of variables and values matter in C#. they are not interchangeable.
 62                * there are eight integer types for storing integers of differing sizes and ranges: int, short, long, byte, sbyte, uint, ushort, and ulong.
